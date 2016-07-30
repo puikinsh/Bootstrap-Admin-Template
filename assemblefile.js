bc74e93dce182bfea966ba0f5c231a84ae5aabb0
Osman Nuri Okumus <onokumus@gmail.com>
2016-07-31 00:35:46 +0300

    created assemblefile.js

---
+++ assemblefile.js
@@ -0,0 +1,45 @@
+'use strict';
+
+var assemble = require('assemble');
+var extname = require('gulp-extname');
+var path = require('path');
+var helpers = require('handlebars-helpers');
+var compare = helpers.comparison();
+var pkg = require('./package');
+
+var app = assemble();
+
+app.task('init', function(cb) {
+    app.helper('md', require('helper-md').sync);
+    app.helper('is', compare);
+    app.helper('markdown', require('helper-markdown'));
+    app.data('pkg', pkg);
+    app.data(['./src/templates/data/**/*.{json,yml}']);
+    // app.data('analytic','UA-23581568-13');
+    app.layouts(path.join(__dirname, './src/templates/layouts/*.hbs'));
+    app.partials(path.join(__dirname, './src/templates/partials/**/*.hbs'));
+    app.pages(path.join(__dirname, './src/templates/pages/*.hbs'));
+    app.option('layout', 'default');
+    cb();
+});
+
+app.task('html:rtl', ['init'], function() {
+    app.data('assets', '../assets');
+    app.data('rtl', true);
+    return app.toStream('pages')
+        .pipe(app.renderFile())
+        .pipe(extname())
+        .pipe(app.dest('public/rtl'));
+});
+
+app.task('html', ['init'], function() {
+    app.data('assets', 'assets');
+    return app.toStream('pages')
+        .pipe(app.renderFile())
+        .pipe(extname())
+        .pipe(app.dest('public'));
+});
+
+app.task('default', ['html', 'html:rtl']);
+
+module.exports = app;
